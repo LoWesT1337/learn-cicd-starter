9c08875f672eebf1d58b9fb9f9c195f35a6c3cad
lowest1337 <e1326220@student.tuwien.ac.at>
2026-04-13 13:06:04 +0200

    db migration into cd workflow

--- scripts/migrateup.sh
+++ scripts/migrateup.sh
@@ -2,7 +2,11 @@
 
 if [ -f .env ]; then
     source .env
+    echo "Env file sourced"
+else
+    echo "Env file not sourced!"
 fi
 
 cd sql/schema
+echo "db url is: $DATABASE_URL"
 goose turso $DATABASE_URL up
